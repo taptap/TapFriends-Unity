d8a5486adfaee3b716b933290fd3a536b6840d3c
zhangwenyan <zhangwenyan@xd.com>
2021-04-29 15:07:30 +0800

    style(*): adjust ui

--- Assets/Scenes/Sample.cs
+++ Assets/Scenes/Sample.cs
@@ -1,29 +1,60 @@
 ﻿using TapTap.Bootstrap;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sample : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Toggle switchButton;
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+      
     }
     
     private bool isSwitch = true;
+    private string channel = "输入TapDB：channel";
+    private string gameVersion = "输入TapDB：gameVersion";
+
+    private string channelValue()
+    {
+        if (channel == "输入TapDB：channel")
+        {
+            return "";
+        }
+        return channel;
+    }
+    private string gameVersionValue()
+    {
+        if (gameVersion == "输入TapDB：gameVersion")
+        {
+            return "";
+        }
+        return gameVersion;
+    }
+
     private void OnGUI()
     {
         GUIStyle style = new GUIStyle(GUI.skin.button);
         style.fontSize = 40;
         
-        // GUIStyle myToggleStyle = new GUIStyle(GUI.skin.toggle)
-        // {
-        //     fontSize = 20
-        // };
-        // GUI.Toggle(new Rect(380, 100, 120, 30), isSwitch, "TapDB开关",myToggleStyle);
+        GUIStyle myToggleStyle = new GUIStyle(GUI.skin.toggle)
+        {
+            fontSize = 35,
+        };
+        GUIStyle inputStyle = new GUIStyle(GUI.skin.textArea);
+        inputStyle.fontSize = 27;
+
+        isSwitch = GUI.Toggle(new Rect(380, 100, 200, 55), isSwitch, "TapDB开关",myToggleStyle);
+        
+        channel = GUI.TextArea(new Rect(380, 180, 330, 70), channel, inputStyle);
+        gameVersion = GUI.TextArea(new Rect(380, 260, 330, 70), gameVersion, inputStyle);
 
         if (GUI.Button(new Rect(60, 100, 280, 100), "RND-IO", style))
         {
@@ -31,9 +62,9 @@ public class Sample : MonoBehaviour
                 .ClientID("uZ8Yy6cSXVOR6AMRPj")
                 .ClientSecret("AVhR1Bu9qfLR1cGbZMAdZ5rzJSxfoEiQaFf1T2P7")
                 .RegionType(RegionType.IO)
-                .TapDBConfig(true, "channel", "gameVersion")
+                .TapDBConfig(isSwitch, channelValue(), gameVersionValue())
                 .Builder();
-
+            
             TapBootstrap.Init(config);
         }
 
@@ -43,7 +74,7 @@ public class Sample : MonoBehaviour
                 .ClientID("uZ8Yy6cSXVOR6AMRPj")
                 .ClientSecret("AVhR1Bu9qfLR1cGbZMAdZ5rzJSxfoEiQaFf1T2P7")
                 .RegionType(RegionType.CN)
-                .TapDBConfig(true, "channel", "gameVersion")
+                .TapDBConfig(isSwitch, channelValue(), gameVersionValue())
                 .Builder();
 
             TapBootstrap.Init(config);
@@ -55,7 +86,7 @@ public class Sample : MonoBehaviour
                 .ClientID("KFV9Pm9ojdmWkkRJeb")
                 .ClientSecret("7mpVJdXIOLQxvQdqjEEpiz7eLf82cMwYkdgoAZqF")
                 .RegionType(RegionType.IO)
-                .EnableTapDB(false)
+                .EnableTapDB(isSwitch)
                 .Builder();
 
             TapBootstrap.Init(config);
@@ -67,7 +98,7 @@ public class Sample : MonoBehaviour
                 .ClientID("0RiAlMny7jiz086FaU")
                 .ClientSecret("8V8wemqkpkxmAN7qKhvlh6v0pXc8JJzEZe3JFUnU")
                 .RegionType(RegionType.CN)
-                .TapDBConfig(true, "channel", "gameVersion")
+                .TapDBConfig(isSwitch, channelValue(), gameVersionValue())
                 .Builder();
             TapBootstrap.Init(config);
         }
