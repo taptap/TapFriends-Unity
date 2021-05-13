8e1344a26f1e1c13d4198416facd83bf7baab38e
xiaoyi <xiaoyi@xd.com>
2021-05-13 14:20:50 +0800

    feat(script): update package script

--- native_mv.sh
+++ native_mv.sh
@@ -12,13 +12,25 @@ releaseAndroid=false
 
 releaseIOS=true
 
+shopt -s extglob
+
 function RemoveNativePackage(){
   if [ $releaseAndroid == true ];then
-      rm -fr $rootPath/Assets/TapTap/$1/Plugins/Android/libs/*
+      rm -fr $rootPath/Assets/TapTap/$1/Plugins/Android/libs/* 
   fi
   
   if [ $releaseIOS == true ];then
+      
+      mkdir $rootPath/Assets/TapTap/$1/Plugins/tempDll/
+      
+      cp $rootPath/Assets/TapTap/$1/Plugins/iOS/TapTap.$1.dll.meta $rootPath/Assets/TapTap/$1/Plugins/tempDll/TapTap.$1.dll.meta
+    
       rm -fr $rootPath/Assets/TapTap/$1/Plugins/iOS/*
+      
+      cp $rootPath/Assets/TapTap/$1/Plugins/tempDll/TapTap.$1.dll.meta $rootPath/Assets/TapTap/$1/Plugins/iOS/TapTap.$1.dll.meta
+      
+      rm -fr $rootPath/Assets/TapTap/$1/Plugins/tempDll
+      
   fi
 }
 
@@ -29,15 +41,13 @@ function CopyNativePackage(){
 for ((i=0;i<${#module[@]};i++));do
   
     RemoveNativePackage ${module[$i]} 
-  
-    echo "Start to Copy ${module[$i]} binary"   
     
+    echo "Start to Copy ${module[$i]} binary"   
+
     aar=Tap${module[$i]}_$version.aar
     bundle=Tap${module[$i]}Resource.bundle
     framework=Tap${module[$i]}SDK.framework
-    
-    
-    
+
     if [ "${module[$i]}" == "TapDB" ];then
         aar=TapDB_$version.aar
         framework=TapDB.framework
@@ -47,28 +57,30 @@ for ((i=0;i<${#module[@]};i++));do
         aar=TapFriend_$version.aar
         framework=TapFriendSDK.framework
     fi
-  
+
     if [ "${module[$i]}" == "FriendsUI" ]; then
         aar=TapFriendUI_$version.aar
         bundle=TapFriendResource.bundle
         framework=TapFriendUISDK.framework
     fi
-  
+
     echo "Android Binary $aar"   
     if [ $releaseAndroid == true ];then
         CopyNativePackage $rootPath/NativePackages/$aar $rootPath/Assets/TapTap/${module[$i]}/Plugins/Android/libs/$aar
     fi
-    
+
     if [ $releaseIOS == true ];then
       if [ "${module[$i]}" == "Bootstrap" ] || [ "${module[$i]}" == "Common" ] || [ "${module[$i]}" == "Moment" ] || [ "${module[$i]}" == "FriendsUI" ];then
           echo "iOS Bundle $bundle"   
           mkdir -p $rootPath/Assets/TapTap/${module[$i]}/Plugins/iOS/Resource 
           CopyNativePackage $rootPath/NativePackages/$bundle  $rootPath/Assets/TapTap/${module[$i]}/Plugins/iOS/Resource/$bundle
       fi
-    
+
       if [ "${module[$i]}" != "License" ];then
           echo "iOS framework $framework"  
           CopyNativePackage $rootPath/NativePackages/$framework $rootPath/Assets/TapTap/${module[$i]}/Plugins/iOS/$framework
       fi
     fi
 done   
+
+sh compile.sh
