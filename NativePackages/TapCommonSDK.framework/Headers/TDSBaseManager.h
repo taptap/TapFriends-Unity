e8dbc0bdb7d4be21855168064611d3f620349324
zhangwenyan <zhangwenyan@xd.com>
2021-05-13 12:04:29 +0800

    feat(*): update framework

---
+++ NativePackages/TapCommonSDK.framework/Headers/TDSBaseManager.h
@@ -0,0 +1,32 @@
+//
+//  TDSSDK.h
+//  TDSCommon
+//
+//  Created by Bottle K on 2020/10/13.
+//
+
+#import <Foundation/Foundation.h>
+#import <TapCommonSDK/TDSAccount.h>
+
+#define TapCommonSDK @"TapCommon"
+#define TapCommonSDK_VERSION_NUMBER @"20101002"
+#define TapCommonSDK_VERSION        @"2.1.1"
+
+NS_ASSUME_NONNULL_BEGIN
+typedef NSString *TDSLanguage NS_STRING_ENUM;
+
+FOUNDATION_EXPORT TDSLanguage const TDSLanguageCN;
+FOUNDATION_EXPORT TDSLanguage const TDSLanguageEN;
+
+@interface TDSBaseManager : NSObject
+
++ (instancetype)new NS_UNAVAILABLE;
+
++ (instancetype)shareInstance;
+
+- (void)setLanguage:(NSString *)language;
+
+- (NSString *)getLanguage;
+@end
+
+NS_ASSUME_NONNULL_END
