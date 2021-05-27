ac96ce6a95b83e3c021436a67aa5cd7fe08d0044
xiaoyi <xiaoyi@xd.com>
2021-05-27 14:19:38 +0800

    feat(bootstrap): add language type

--- TapSDK/TapBootstrapSDK/TapLanguage.cs
+++ TapSDK/TapBootstrapSDK/TapLanguage.cs
@@ -2,7 +2,13 @@ namespace TapTap.Bootstrap
 {
     public enum TapLanguage
     {
-        ZH_HANS,
-        EN
+        AUTO = 0,
+        ZH_HANS = 1,
+        EN = 2,
+        ZH_HANT = 3,
+        JA = 4,
+        KO = 5,
+        TH = 6,
+        ID = 7,
     }
 }
