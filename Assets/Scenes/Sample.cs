40d1430c00e6074e291e806cd9e229b85242c3fe
xiaoyi <xiaoyi@xd.com>
2021-05-28 14:23:21 +0800

    feat: scene add language

--- Assets/Scenes/Sample.cs
+++ Assets/Scenes/Sample.cs
@@ -26,6 +26,7 @@ public class Sample : MonoBehaviour
     private bool isIDFA = true;
     private string channel = "输入TapDB：channel";
     private string gameVersion = "输入TapDB：gameVersion";
+    private string language = "0";
 
     private string channelValue()
     {
@@ -60,15 +61,18 @@ public class Sample : MonoBehaviour
         GUIStyle inputStyle = new GUIStyle(GUI.skin.textArea);
         inputStyle.fontSize = 27;
 
-        isSwitch = GUI.Toggle(new Rect(380, Judge.IsIphoneXDevice?100:0, 200, 55), isSwitch, "TapDB开关", myToggleStyle);
+        isSwitch = GUI.Toggle(new Rect(380, Judge.IsIphoneXDevice ? 100 : 0, 200, 55), isSwitch, "TapDB开关",
+            myToggleStyle);
 
-        isIDFA = GUI.Toggle(new Rect(380, Judge.IsIphoneXDevice?200:60, 200, 55), isIDFA, "IDFA 开关", myToggleStyle);
+        isIDFA = GUI.Toggle(new Rect(380, Judge.IsIphoneXDevice ? 200 : 60, 200, 55), isIDFA, "IDFA 开关", myToggleStyle);
 
-        channel = GUI.TextArea(new Rect(380, Judge.IsIphoneXDevice?280:135, 330, 70), channel, inputStyle);
+        channel = GUI.TextArea(new Rect(380, Judge.IsIphoneXDevice ? 280 : 135, 330, 70), channel, inputStyle);
 
-        gameVersion = GUI.TextArea(new Rect(380, Judge.IsIphoneXDevice?360:215, 330, 70), gameVersion, inputStyle);
+        gameVersion = GUI.TextArea(new Rect(380, Judge.IsIphoneXDevice ? 360 : 215, 330, 70), gameVersion, inputStyle);
 
-        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice?100:0, 280, 100), "RND-IO", style))
+        language = GUI.TextArea(new Rect(380, Judge.IsIphoneXDevice ? 480 : 295, 330, 70), language, inputStyle);
+
+        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice ? 100 : 0, 280, 100), "RND-IO", style))
         {
             var config = new TapConfig.Builder()
                 .ClientID("uZ8Yy6cSXVOR6AMRPj")
@@ -76,13 +80,13 @@ public class Sample : MonoBehaviour
                 .RegionType(RegionType.IO)
                 .TapDBConfig(isSwitch, channelValue(), gameVersionValue(), isIDFA)
                 .ConfigBuilder();
-            
+
             TapDB.AdvertiserIDCollectionEnabled(isIDFA);
-            
+
             TapBootstrap.Init(config);
         }
 
-        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice?250:150, 280, 100), "RND-CN", style))
+        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice ? 250 : 150, 280, 100), "RND-CN", style))
         {
             var config = new TapConfig.Builder()
                 .ClientID("uZ8Yy6cSXVOR6AMRPj")
@@ -92,11 +96,11 @@ public class Sample : MonoBehaviour
                 .ConfigBuilder();
 
             TapDB.AdvertiserIDCollectionEnabled(isIDFA);
-            
+
             TapBootstrap.Init(config);
         }
 
-        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice?400:300, 280, 100), "海外", style))
+        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice ? 400 : 300, 280, 100), "海外", style))
         {
             var config = new TapConfig.Builder()
                 .ClientID("KFV9Pm9ojdmWkkRJeb")
@@ -108,40 +112,46 @@ public class Sample : MonoBehaviour
             TapBootstrap.Init(config);
         }
 
-        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice?550:450, 280, 100), "国内", style))
+        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice ? 550 : 450, 280, 100), "国内", style))
         {
             var config = new TapConfig.Builder()
                 .ClientID("0RiAlMny7jiz086FaU")
                 .ClientSecret("8V8wemqkpkxmAN7qKhvlh6v0pXc8JJzEZe3JFUnU")
                 .RegionType(RegionType.CN)
-                .TapDBConfig(isSwitch, channelValue(), gameVersionValue(),isIDFA)
+                .TapDBConfig(isSwitch, channelValue(), gameVersionValue(), isIDFA)
                 .ConfigBuilder();
             TapBootstrap.Init(config);
         }
 
-        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice?700:600, 280, 100), "登陆", style))
+        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice ? 700 : 600, 280, 100), "登陆", style))
         {
             UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(1);
         }
 
-        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice?850:750, 280, 100), "动态", style))
+        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice ? 850 : 750, 280, 100), "动态", style))
         {
             UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(2);
         }
 
-        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice?1000:900, 280, 100), "TapDB", style))
+        if (GUI.Button(new Rect(60, Judge.IsIphoneXDevice ? 1000 : 900, 280, 100), "TapDB", style))
         {
             UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(3);
         }
 
-        if (GUI.Button(new Rect(380, Judge.IsIphoneXDevice?470:325, 280, 100), "TapFriend", style))
+        if (GUI.Button(new Rect(380, Judge.IsIphoneXDevice ? 570 : 425, 280, 100), "TapFriend", style))
         {
             UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(4);
         }
 
-        if (GUI.Button(new Rect(380, Judge.IsIphoneXDevice?610:465, 280, 100), "Common", style))
+        if (GUI.Button(new Rect(380, Judge.IsIphoneXDevice ? 710 : 565, 280, 100), "Common", style))
         {
             UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(5);
+        }
+
+        if (GUI.Button(new Rect(380, Judge.IsIphoneXDevice ? 850 : 715, 280, 100), "设置语言", style))
+        {
+            var languageType = int.Parse(language);
+            TapBootstrap.SetPreferLanguage((TapLanguage) languageType);
         }
     }
 }
