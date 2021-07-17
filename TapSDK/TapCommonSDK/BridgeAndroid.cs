1d650c7c5c19d2e059b139f8b1e018d3a1a31c78
xiaoyi <xiaoyi@xd.com>
2021-07-17 20:22:00 +0800

    fix: fix clz not found exception

--- TapSDK/TapCommonSDK/BridgeAndroid.cs
+++ TapSDK/TapCommonSDK/BridgeAndroid.cs
@@ -12,60 +12,55 @@ namespace TapTap.Common
         private string registerHandlerMethod = "registerHandler";
 
         private string callHandlerMethod = "callHandler";
-        
+
         private string initMethod = "init";
 
         private string registerMethod = "register";
 
-        private AndroidJavaObject mCurrentActivity;
+        private readonly AndroidJavaObject _mAndroidBridge;
 
-        private AndroidJavaObject mAndroidBridge;
-
-        private static BridgeAndroid sInstance = new BridgeAndroid();
+        private static readonly BridgeAndroid SInstance = new BridgeAndroid();
 
         public static BridgeAndroid GetInstance()
         {
-            return sInstance;
+            return SInstance;
         }
 
         private BridgeAndroid()
         {
-            mCurrentActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
-            mAndroidBridge = new AndroidJavaClass(bridgeJavaClz).CallStatic<AndroidJavaObject>(instanceMethod);
-            mAndroidBridge.Call(initMethod, mCurrentActivity);
+            var mCurrentActivity =
+                new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+            _mAndroidBridge = new AndroidJavaClass(bridgeJavaClz).CallStatic<AndroidJavaObject>(instanceMethod);
+            _mAndroidBridge.Call(initMethod, mCurrentActivity);
         }
 
         public void Register(string serviceClzName, string serviceImplName)
         {
-            if (mAndroidBridge == null)
+            if (_mAndroidBridge == null)
             {
                 return;
             }
 
-            AndroidJavaClass serviceClass = new AndroidJavaClass(serviceClzName);
-            AndroidJavaObject serviceImpl = new AndroidJavaObject(serviceImplName);
-            mAndroidBridge.Call(registerMethod, serviceClass, serviceImpl);
+            try
+            {
+                var serviceClass = new AndroidJavaClass(serviceClzName);
+                var serviceImpl = new AndroidJavaObject(serviceImplName);
+                _mAndroidBridge.Call(registerMethod, serviceClass, serviceImpl);
+            }
+            catch (Exception)
+            {
+                //
+            }
         }
 
         public void Call(Command command, Action<Result> action)
         {
-            if (mAndroidBridge == null)
-            {
-                return;
-            }
-
-            mAndroidBridge.Call(registerHandlerMethod, command.ToJSON(), new BridgeCallback(action));
+            _mAndroidBridge?.Call(registerHandlerMethod, command.ToJSON(), new BridgeCallback(action));
         }
 
         public void Call(Command command)
         {
-            if (mAndroidBridge == null)
-            {
-                return;
-            }
-
-            mAndroidBridge.Call(callHandlerMethod, command.ToJSON());
+            _mAndroidBridge?.Call(callHandlerMethod, command.ToJSON());
         }
-    
     }
 }
