5d4daae8b32503ea2ecc052eb9fa1e438d95e3b9
xiaoyi <xiaoyi@xd.com>
2021-08-12 13:58:44 +0800

    feat: add test

---
+++ .ci/test.sh
@@ -0,0 +1,26 @@
+# 生成测试报告 xml
+
+resultContent=$(java -jar ./.ci/release.jar message --title="${CI_PROJECT_TITLE} build " --body="<${CI_JOB_URL}|Package Start>")
+echo $resultContent
+THREAD=${resultContent#*threadTs=}
+
+cd ./TapSDK
+
+dotnet test /p:CollectCoverage=true /p:CoverletOutput='./results/' /p:CoverletOutputFormat=opencover
+
+# 测试模块
+module=("TapBootstrapSDK" "TapGameSnapshot")
+
+for element in ${module[@]}
+do
+    dotnet reportgenerator -reports:$element/$element.Test/results/coverage.opencover.xml -targetdir:$element/$element.Test/results
+    
+    zip -q -ry TestReport_$element.zip $element/$element.Test/results
+    
+    cd ..
+    
+    java -jar .ci/release.jar nb --af=TapSDK/TestReport_$element.zip --thread $THREAD
+    
+    cd ./TapSDK
+    
+done
